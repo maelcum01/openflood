c8aebe1bbc6f1affbb52f38972ab6ff8bdf97576
maelcum01 <38456895+maelcum01@users.noreply.github.com>
2018-06-24 13:33:02 +0200

    beta version

--- web/main.dart
+++ web/main.dart
@@ -179,9 +179,13 @@ class BoardModel
   }
 }
 
+/* The View Class:
+    Initializes and renders the DOM and HTML elements
+    The constructors parameters are the boards size
+    and a list of the colors present in each game or level.
+ */
 class BoardView
 {
-  var tileViews;
   var rootElem = null;
   var boardElem = null;
   var width = 50.0;
@@ -204,7 +208,7 @@ class BoardView
 
   init()
   {
-    this.rootElem = querySelector('#main');
+    this.rootElem = querySelector('#main'); // set root to #main element
     rootElem.children.clear();
     rootElem.style.width = "360px";
 
@@ -212,20 +216,20 @@ class BoardView
     this.titleBar.innerHtml = "";
     rootElem.children.add(titleBar);
 
-    this.width = double.parse(rootElem.style.width.replaceAll("px", ""))/x; // pixel per tile
+    this.width = double.parse(rootElem.style.width.replaceAll("px", ""))/x; // calculate pixel per tile
     this.height = width;
-    this.boardElem = new Element.div();
+    this.boardElem = new Element.div(); // create game board element within the root element
     rootElem.children.add(boardElem);
 
-    for(var rowY = 0; rowY< y;rowY++)
+    for(var rowY = 0; rowY< y;rowY++) // create y rows of div elements within the board div element
     {
       var rowElem = new Element.div();
       boardElem.children.add(rowElem);
-      for (var tileX = 0; tileX < x;tileX++)
+      for (var tileX = 0; tileX < x;tileX++) // create x tiles of div elements within each row div element
       {
-        var tileElem = new DivElement();
+        var tileElem = new Element.div();
         rowElem.append(tileElem);
-        tileElem.style.display = "inline-block";
+        tileElem.style.display = "inline-block";  // set style parameters for each tile
         tileElem.style.border = "solid 1px grey";
         tileElem.style.width = (width * 0.95).toString()+"px";
         tileElem.style.height = height.toString()+"px";
@@ -234,15 +238,15 @@ class BoardView
     }
     this.gameInfo = new Element.div();
     rootElem.children.add(gameInfo);
-    this.buttonBar = new Element.div();
+    this.buttonBar = new Element.div(); // create button bar div element
     this.buttonBar.classes.add("buttonBar");
-    rootElem.children.add(buttonBar);
-    for(var color in this.colors)
+    rootElem.children.add(buttonBar); // append button bar to root div element
+    for(var color in this.colors)     // create button elements of each color from json list, within the button bar div element
     {
       var colorButton = new ButtonElement();
       colorButton.classes.add("colorButton");
       buttonBar.children.add(colorButton);
-      colorButton.style.backgroundColor= color;
+      colorButton.style.backgroundColor= color; // set background to json list color
       colorButton.style.width =(width * 0.95).toString()+"px";
       colorButton.style.height= height.toString()+"px";
     }
