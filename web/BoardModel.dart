97cf89a58ea09aebba16ff9aa5a14b724dddf311
maelcum01 <38456895+maelcum01@users.noreply.github.com>
2018-06-24 13:48:26 +0200

    beta version

---
+++ web/BoardModel.dart
@@ -0,0 +1,58 @@
+part of openflood;
+
+
+
+class BoardModel
+{
+  var x,y;
+  var colors = [];
+  var tiles = [];
+  var level = 0;
+  var maxSteps = 0;
+  var score = 0;
+
+  BoardModel(var level, var size, var colors, var initialTiles, var maxSteps)
+  {
+    this.level = level;
+    this.x = size;
+    this.y = size;
+    this.colors = colors;
+    tiles = initialTiles;
+    this.maxSteps = maxSteps;
+  }
+
+  setColor(var newColor)
+  {
+    var oldColor = tiles[0][0];
+    for(var i = 0; i < tiles.length; i++)
+    {
+      for(var j = 0; j < tiles[i].length; j++)
+      {
+        if(tiles[i][j] == oldColor)
+        {
+          tiles[i][j] = newColor;
+          score +=2;
+        }
+        else
+        {
+          break;
+        }
+      }
+    }
+  }
+  checkWin()
+  {
+    var color = tiles[0][0];
+    for(var i = 0; i <tiles.length; i++)
+    {
+      for(var j = 0; j < tiles[i].length; j++)
+      {
+        if(tiles[i][j] != color)
+        {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+}
